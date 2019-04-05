b9493d9e6035838858d2342985ff63740a8ca8d9
ShuyangLiu <liushuyang2009@gmail.com>
2019-04-04 18:05:06 -0700

    add more

---
+++ SB+mfence/src/P0.java
@@ -0,0 +1,62 @@
+import java.lang.invoke.MethodHandles;
+import java.lang.invoke.VarHandle;
+import java.util.concurrent.BrokenBarrierException;
+import java.util.concurrent.CyclicBarrier;
+
+public class P0 extends Thread
+{
+    private CyclicBarrier signal;
+    private CyclicBarrier done;
+
+    private Point p;
+
+    Object EAX;
+
+    private static VarHandle X;
+    static {
+        try {
+            X = MethodHandles.lookup()
+                    .findVarHandle(Point.class, "x", int.class);
+        } catch (NoSuchFieldException | IllegalAccessException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static VarHandle Y;
+    static {
+        try {
+            Y = MethodHandles.lookup()
+                    .findVarHandle(Point.class, "y", int.class);
+        } catch (NoSuchFieldException | IllegalAccessException e) {
+            e.printStackTrace();
+        }
+    }
+
+    P0(Point p, CyclicBarrier sig, CyclicBarrier done) {
+        EAX = 0;
+        signal = sig;
+        this.done = done;
+        this.p = p;
+    }
+
+    public void run() {
+        try {
+            signal.await();
+        } catch (InterruptedException | BrokenBarrierException e) {
+            e.printStackTrace();
+        }
+
+        //MOV [x],$1
+        // MFENCE
+        //MOV EAX,[y]
+        X.setOpaque(p, 1);
+        VarHandle.fullFence();
+        EAX = Y.getOpaque(p);
+
+        try {
+            done.await();
+        } catch (InterruptedException | BrokenBarrierException e) {
+            e.printStackTrace();
+        }
+    }
+}
